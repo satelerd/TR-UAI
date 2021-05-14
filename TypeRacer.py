9ee5e856fdd121a37e20aa5f10d6681d2c57990e
satelerd <satelerd@gmail.com>
2021-05-13 20:24:06 -0400

    FPS

--- TypeRacer.py
+++ TypeRacer.py
@@ -6,11 +6,22 @@ pygame.init()
 # Historia
 historia = "daniel matias"
 
-
 # Window
 size = width, height = 1100, 700
 win = pygame.display.set_mode(size)
 pygame.display.set_caption("TypeRacer")
+
+FPS = 60
+
+# Colores
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+# HUD
+def draw_window():
+    win.fill(WHITE)
+    pygame.draw.rect(win, (BLACK), pygame.Rect(0, 0, 300, height))
+    pygame.display.update()
 
 
 # Funciones
@@ -22,17 +33,16 @@ def letraIncorrecta():
 def letraCorrecta():
     print("Correcto")
 
-
+clock = pygame.time.Clock()
 run = True
 contador = 0
 while run:
+    clock.tick(FPS)
     pygame.time.delay(100)
 
     keys = pygame.key.get_pressed()
-
     # Loop de eventos
     for event in pygame.event.get():
-
         # Levanto una tecla
         if event.type == pygame.KEYUP:
             tecla = pygame.key.name(event.key)
@@ -44,10 +54,9 @@ while run:
         # Salir
         if event.type == pygame.QUIT:
             run = False
-
     
+    draw_window()
     
-    # inicioJuego(keys)
 
 
 pygame.quit()
